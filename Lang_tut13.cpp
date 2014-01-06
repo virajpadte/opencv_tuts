f1da3abd7c2e17ebecc3837b516cbbed956b77f8
viraj padte <virajpadte@gmail.com>
2014-01-06 12:56:49 -0800

    Tut13: Effecient scanning for continuous and padded images

--- Lang_tut13.cpp
+++ Lang_tut13.cpp
@@ -2,10 +2,37 @@
 #include <opencv2\opencv.hpp>
 #include <opencv2\highgui\highgui.hpp>
 
-
 //Delaring namespaces
 using namespace cv;
 using namespace std;
+
+
+//Declaring functions:
+Mat colour_reduce(Mat img,int factor);
+
+//defining functions:
+
+Mat colour_reduce(Mat img,int factor){
+    int case_flag = 0;
+    if(img.isContinuous()){
+        case_flag = 1;
+    }
+    else{
+        case_flag = 2;
+    }
+
+    switch(case_flag){
+        case 1:{
+            cout<<"Continuous flag set"<<endl;
+            break;
+        }
+        case 2:{
+            cout<<"Continuous flag reset"<<endl;
+            break;
+        }
+    }
+    return img;
+}
 
 //Main function:
 
@@ -13,11 +40,12 @@ int main(){
     Mat img = imread("sample_img.jpg");
     cout<<"Padding flag: "<<img.isContinuous()<<endl;
     if(img.isContinuous()){
-        cout<<"\n\nThe image is padded"<<endl;
-    }
-    else{
         cout<<"\n\nThe image is not padded"<<endl;
     }
+    else{
+        cout<<"\n\nThe image is padded"<<endl;
+    }
+    colour_reduce(img,64);
     return 0;
 }
 
